da5a4720625dc12771ec3d8fafc743c7b862d283
AntoineLT <antoine.latorre@digitalairways.com>
2016-07-21 06:49:47 +0000

    Optimisation of the addNodeToServerFlows method

--- js/flows.js
+++ js/flows.js
@@ -19,15 +19,18 @@ function writeFlows(flowsLocation, result) {
 
 function addNodeToServerFlows(node) {
     var content = readFlows(),
-        contentL = content.length,
-        test = false;
+        contentL = content.length;
 
-    for(var i = 0; i < contentL; i++) {
-        if(!node.id) node.id = (1+Math.random()*4294967295).toString(16);
-        if(content[i].id === node.id && content[i].type === node.type)
-            test = true;
+    if(!node.id) {
+        node.id = (1+Math.random()*4294967295).toString(16);
+        content.push(node);
+    } else {
+        for(var i = 0; i < contentL; i++) {
+            if(content[i].id === node.id && content[i].type === node.type)
+                break;
+        }
+        if(i === contentL) content.push(node);
     }
-    if(!test) content.push(node);
     if(contentL < content.length) {
         var result = JSON.stringify(content, null, 4);
         writeFlows(flowsLocation, result);
