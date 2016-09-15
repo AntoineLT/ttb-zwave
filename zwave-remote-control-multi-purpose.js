ceb1b877f5003080447a982e26fc2a5d1708235e
AntoineLT <antoine.latorre@digitalairways.com>
2016-09-15 16:29:30 +0200

    Change zwave-remote-control-multi-purpose.js to look similar to other specifics node

--- zwave-remote-control-multi-purpose.js
+++ zwave-remote-control-multi-purpose.js
@@ -8,32 +8,35 @@ module.exports = function (RED) {
 
     var flows = require('./js/flows');
 
-    function remoteControlMultiPurpose(config) {
+    function main(config) {
         RED.nodes.createNode(this, config);
-        var zwaveTopic = flows.checkZwaveNodeTopic();
-        this.nodeid = config.nodeid;
-        this.push = config.push;
-        this.topic = zwaveTopic + '/' + this.nodeid + '/scene';
-        this.broker = config.broker;
-        this.brokerConn = RED.nodes.getNode(this.broker);
-        var node = this;
-        node.mqtt = mqttCP.get(
-            node.brokerConn.broker,
-            node.brokerConn.port
-        );
-        if (node.brokerConn) {
-            subscription(RED, node);
-        } else {
-            this.error(RED._("mqtt.errors.missing-config"));
+        this.config = config;
+
+        this.brokerConn = RED.nodes.getNode(config.broker);
+        if (this.brokerConn === undefined || this.brokerConn === null) {
+            this.error(RED._("node-red:mqtt.errors.missing-config"));
+            return;
         }
+
+        var zwaveTopic = flows.checkZwaveNodeTopic();
+        this.topic = zwaveTopic + '/' + config.nodeid + '/scene';
+
+        this.mqtt = mqttCP.get(
+            this.brokerConn.broker,
+            this.brokerConn.port
+        );
+
+        subscription(RED, this);
     }
 
-    RED.nodes.registerType("zwave-remote-control-multi-purpose", remoteControlMultiPurpose);
+    RED.nodes.registerType("zwave-remote-control-multi-purpose", main);
 };
 
 function subscription(RED, node) {
     var isUtf8 = require('is-utf8');
-    var msg;
+
+    var msg = {};
+
     if (node.topic) {
         node.brokerConn.register(node);
         node.brokerConn.subscribe(node.topic, 2, function (topic, payload, packet) {
@@ -41,16 +44,12 @@ function subscription(RED, node) {
                 payload = payload.toString();
             }
             try {
-                msg = JSON.parse(payload);
+                msg.payload = JSON.parse(payload);
             } catch (e) {
-                node.error(e);
+                msg.payload = payload;
             }
-            if (typeof msg !== 'object') {
-                msg = {
-                    payload: msg || payload
-                };
-            }
-            softRemote(node, payload);
+
+            softRemote(node, msg.payload);
         }, node.id);
     }
     else {
@@ -72,36 +71,36 @@ function softRemote(node, sceneID) {
     var msgMQTT = {
             qos: 0,
             retain: true,
-            topic: topic + '/' + node.nodeid + '/out'
+            topic: topic + '/' + node.config.nodeid + '/out'
         },
         msg = {
             payload: sceneID
         };
     switch (sceneID) {
-        case "10":
+        case 10:
             msg.intent = 1; // close
             break;
 
-        case "20":
+        case 20:
             msg.intent = 2; // more
             break;
 
-        case "30":
+        case 30:
             msg.intent = 0; // open
             break;
 
-        case "40":
+        case 40:
             msg.intent = 3; // less
             break;
 
-        case "21":
-        case "41":
+        case 21:
+        case 41:
             clearTimeout(timer);
             count = 0;
             break;
 
-        case "22":
-            if (node.push === true) {
+        case 22:
+            if (node.config.push === true) {
                 count++;
                 if (count >= 20) break;
                 msg.intent = 2; // more
@@ -117,8 +116,8 @@ function softRemote(node, sceneID) {
             }
             break;
 
-        case "42":
-            if (node.push === true) {
+        case 42:
+            if (node.config.push === true) {
                 count++;
                 if (count >= 20) break;
                 msg.intent = 3; // less
